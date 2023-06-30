ddd2218e07726d6ee02021f62bbbfb2d30494702
drew <drewbeland@gmail.com>
2023-06-30 16:08:33 -0700

    redid up to randomize array

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,24 @@ import RankingButton from './RankingButton'
 export default function App() {
 
   const [unrankedArray, setUnrankedArray] = React.useState([])
-  const [randomziedArray, setRandomziedArray] = React.useState([])
-  const [currentPair, setCurrentPair] = React.useState([])
-  const [rankedArray, setRankedArray] = React.useState([])
-  const [minRange, setMinRange] = React.useState()
-  const [maxRange, setMaxRange] = React.useState()
+  const [randomizedArray, setRandomizedArray] = React.useState()
+
+  React.useEffect( () => console.log(randomizedArray), [randomizedArray])
 
 
-  function handleSubmit(e){
+  function handleIdeaSubmit(e){
     e.preventDefault()
-    const newNameIdea = e.target.nameIdea.value
-    setUnrankedArray(oldArray => [...oldArray, newNameIdea])
+    const idea = e.target.nameIdea.value
+    setUnrankedArray(prevUnrankedArray => {
+      return [...prevUnrankedArray, idea]
+    })
     document.querySelector('#nameIdea').value = ''
   }
 
-  React.useEffect(() => console.table(unrankedArray), [unrankedArray])
-  React.useEffect(() => console.table(randomziedArray), [randomziedArray])
-  React.useEffect(() => console.table(rankedArray), [rankedArray])
-
-
-  function handleNextStep(){
+  function randomizeList(){
     const copyUnrankedArray = [...unrankedArray]
     const shuffledArray = shuffleArrayOrder(copyUnrankedArray)
-    setRandomziedArray(shuffledArray)
-    showNextPair()
+    setRandomizedArray([...shuffledArray])
   }
 
   function shuffleArrayOrder(array){
@@ -40,58 +34,6 @@ export default function App() {
     return array;
   }
 
-  // React.useEffect(() => showNextPair(), [])
-
-  const rankingElements = currentPair.map(e => <RankingButton key={e.id} value={e.value} maxRange={e.maxRange} minRange={e.minRange} rankedIndex={e.rankedIndex} handleChoice={() => handleChoice(e.value)}/>)
-
-  function showNextPair(){
-    if(rankedArray.length === 0){
-      const pair = randomziedArray.slice(0,2)
-      let firstPairObjectsArray = []
-      for(let i = 0; i < pair.length; i++){
-        firstPairObjectsArray.push({
-          value: pair[i],
-          id: nanoid(),
-          maxRange: rankedArray.length,
-          minRange: 0
-        })
-      }
-      setCurrentPair(firstPairObjectsArray)
-      setRandomziedArray(randomziedArray.slice(2))
-      console.log(firstPairObjectsArray)
-    } else{
-      let pairObjectsArray = []
-      console.log(pairObjectsArray)
-      let currentlyRanking = {
-        value: randomziedArray.pop(),
-        id: nanoid(),
-        maxRange: rankedArray.length-1,
-        minRange: 0
-      }
-      pairObjectsArray.push(currentlyRanking)
-      let rankedItem = {
-        id: nanoid(),
-        rankedIndex: Math.floor((rankedArray.length-1)/2),
-        value: rankedArray[Math.floor((rankedArray.length-1)/2)],
-      }
-      pairObjectsArray.push(rankedItem)
-      setCurrentPair(pairObjectsArray)
-      console.log(pairObjectsArray)
-  }}
-
-  function handleChoice(choice){
-    const chosenOption = currentPair.find(element => element.value === choice);
-    const unchosenOption = currentPair.find(element => element.value !== choice);
-    if(rankedArray.length === 0){
-      setRankedArray([chosenOption, unchosenOption])
-      setMinRange(rankedArray.indexOf(chosenOption))
-      setMaxRange(rankedArray.indexOf(unchosenOption+1))
-    }else{
-      showNextPair()
-      setUnrankedArray(prevUnrankedArray => prevUnrankedArray.slice(1))
-    }
-  }
-
 
   return (
     <>
@@ -99,17 +41,11 @@ export default function App() {
         <h1>Submit as many name ideas as you can</h1>
         <h2>Three bouts of ten minutes is all you get. Then move on to the next step</h2>
       </section>
-      <form action="" onSubmit={handleSubmit}>
-        <label htmlFor="nameIdea">Startup Name:</label>
-        <input name='nameIdea' id='nameIdea' type="text" />
-        <input type="submit" />
+      <form onSubmit={handleIdeaSubmit}> 
+        <input name='nameIdea' id='nameIdea' type="text" placeholder='Name Idea' />
+        <button type="submit" >Submit!</button>
       </form>
-      <button onClick={handleNextStep}>Next Step!</button>
-
-      <section className='rankArea'>
-        <h2>Rank Names</h2>
-        {rankingElements}
-      </section>
+      <button onClick={randomizeList}>Randomize Order</button>
     </>
   )
 }
