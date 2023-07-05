704f70bd46bdcbd45e81a02885e059916e23d0d8
drew <drewbeland@gmail.com>
2023-07-05 12:35:21 -0700

    setting up buttons to first comparison working

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ import RankingButton from './RankingButton'
 export default function App() {
 
   const [unrankedArray, setUnrankedArray] = React.useState([])
-  const [randomizedArray, setRandomizedArray] = React.useState()
+  const [randomizedArray, setRandomizedArray] = React.useState([])
+  const [currentPair, setCurrentPair] = React.useState([])
+  const [rankedArray, setRankedArray] = React.useState([])
 
+  React.useEffect( () => console.log(unrankedArray), [unrankedArray])
   React.useEffect( () => console.log(randomizedArray), [randomizedArray])
+  React.useEffect( () => console.log(currentPair), [currentPair])
+  React.useEffect( () => console.log(rankedArray), [rankedArray])
 
 
   function handleIdeaSubmit(e){
@@ -20,10 +25,16 @@ export default function App() {
     document.querySelector('#nameIdea').value = ''
   }
 
-  function randomizeList(){
-    const copyUnrankedArray = [...unrankedArray]
+  function handleRandomize(){
+    setRandomizedArray(randomizeList(unrankedArray))
+    // setRankedArray(randomizedArray[0])
+  }
+
+  function randomizeList(array){
+    const copyUnrankedArray = [...array]
     const shuffledArray = shuffleArrayOrder(copyUnrankedArray)
-    setRandomizedArray([...shuffledArray])
+    return shuffledArray
+    // setRandomizedArray([...shuffledArray])
   }
 
   function shuffleArrayOrder(array){
@@ -33,6 +44,33 @@ export default function App() {
     }
     return array;
   }
+
+  function handleSetRankedArray(){
+    setRankedArray(randomizedArray[0])
+    setRandomizedArray(prevRandomizedArray => prevRandomizedArray.slice(1))
+
+  }
+
+  function handleShowFirstCard(){
+    setCurrentPair([
+      {
+        value: randomizedArray[0],
+        id: nanoid()
+      },
+      {
+        value: rankedArray[0],
+        id: nanoid()
+      }
+    ])
+  }
+
+
+  const rankingElements = currentPair.map(e => {
+    return <RankingButton 
+      value={e.value}
+      key={e.id}
+    />
+  })
 
 
   return (
@@ -45,7 +83,10 @@ export default function App() {
         <input name='nameIdea' id='nameIdea' type="text" placeholder='Name Idea' />
         <button type="submit" >Submit!</button>
       </form>
-      <button onClick={randomizeList}>Randomize Order</button>
+      <button onClick={handleRandomize}>Randomize Order</button>
+      <button onClick={handleSetRankedArray}>Set ranked Array</button>
+      <button onClick={handleShowFirstCard}>Show First Cards</button>
+      {rankingElements}
     </>
   )
 }
