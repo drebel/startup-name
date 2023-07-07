919aac0a624d03169b4dfe2666cb45bc1468ca11
drew <drewbeland@gmail.com>
2023-07-07 13:48:39 -0700

    back on branch, ranking working, need terminal condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,70 @@
 import React from 'react'
 import './App.css'
-import {nanoid, random} from 'nanoid'
+import {nanoid} from 'nanoid'
 import RankingButton from './RankingButton'
 
 export default function App() {
 
   const [unrankedArray, setUnrankedArray] = React.useState([])
-  // const [randomizedArray, setRandomizedArray] = React.useState([]) // does this need to be state? no
-  let randomizedArray = []
-
-  const [randomizedIndex, setUnrandomizededIndex] = React.useState(1)
-  const [currentPair, setCurrentPair] = React.useState([]) // yes? can this be an index referencing the randomized array?
-  const [rankedArray, setRankedArray] = React.useState([]) // it will get rendered eventually, so yes?
-  const [minRange, setMinRange] = React.useState(0) // maybe the ranges can be not state since they arent getting rendered. its just a calculation
+  const [randomizedArray, setRandomizedArray] = React.useState([])
+  const [currentPair, setCurrentPair] = React.useState([])
+  const [rankedArray, setRankedArray] = React.useState([])
+  const [minRange, setMinRange] = React.useState(0)
   const [maxRange, setMaxRange] = React.useState(1)
+  const [newCard, setNewCard] = React.useState(true)
 
   React.useEffect( () => console.log(unrankedArray), [unrankedArray])
-  // React.useEffect( () => console.log(randomizedArray), [randomizedArray])
+  React.useEffect( () => console.log(randomizedArray), [randomizedArray])
   React.useEffect( () => console.log(currentPair), [currentPair])
   React.useEffect( () => console.log(rankedArray), [rankedArray])
+  React.useEffect( () => console.log(minRange, maxRange), [minRange, maxRange])
+
+
+  React.useEffect( () => {
+    if(maxRange === minRange ){
+      console.log('step 1a')
+
+      const newRankedArray = [...rankedArray]
+      newRankedArray.splice(minRange, 0, currentPair[0].value)
+      setRankedArray(newRankedArray)
+      setRandomizedArray(prevRandomizedArray => prevRandomizedArray.slice(1))
+    }
+  }, [minRange, maxRange])
+
+  React.useEffect( () => {
+    if(maxRange === minRange && rankedArray.length > 1){
+      console.log('step 2')
+
+      setMinRange(0)
+      setMaxRange(rankedArray.length)
+      const range = rankedArray.length - 0
+      const rangeMidpoint = Math.floor(range / 2) + 0
+      const newPair = [{
+          value: randomizedArray[0],
+          id: nanoid()
+        },
+        {
+          value: rankedArray[rangeMidpoint],
+          id: nanoid()
+        }]
+      setCurrentPair(newPair)
+      setNewCard(true)
+    }
+  }, [rankedArray])
+  
+  React.useEffect( () => {
+    if(rankedArray.length > 1 && maxRange !== minRange && !newCard){
+      console.log('step 1b')
+      const range = maxRange - minRange
+      const rangeMidpoint = Math.floor(range / 2) + minRange
+      const newPair = [...currentPair]
+      newPair[1] = {
+        value: rankedArray[rangeMidpoint],
+        id: nanoid()
+      }
+      setCurrentPair(newPair)
+    }
+  }, [maxRange, minRange])
 
 
   function handleIdeaSubmit(e){
@@ -31,10 +77,8 @@ export default function App() {
   }
 
   function handleRandomize(){
-    // setRandomizedArray(randomizeList(unrankedArray))
+    setRandomizedArray(randomizeList(unrankedArray))
     // setRankedArray(randomizedArray[0])
-    randomizedArray = randomizeList(unrankedArray)
-    console.log(randomizedArray)
   }
 
   function randomizeList(array){
@@ -53,68 +97,52 @@ export default function App() {
   }
 
   function handleSetRankedArray(){
-    setRankedArray(randomizedArray[0])
-    // setRandomizedArray(prevRandomizedArray => prevRandomizedArray.slice(1))
+    setRankedArray([randomizedArray[0]])
+    setRandomizedArray(prevRandomizedArray => prevRandomizedArray.slice(1))
 
   }
 
   function handleShowFirstCard(){
-    // setCurrentPair([
-    //   {
-    //     value: randomizedArray[randomizedIndex],
-    //     id: nanoid()
-    //   },
-    //   {
-    //     value: rankedArray[0],
-    //     id: nanoid()
-    //   }
-    // ])
-    console.log(randomizedArray)
+    setCurrentPair([
+      {
+        value: randomizedArray[0],
+        id: nanoid()
+      },
+      {
+        value: rankedArray[0],
+        id: nanoid()
+      }
+    ])
   }
 
 
   const rankingElements = currentPair.map(e => {
     return <RankingButton 
       value={e.value}
-      key={e.id}
+      key={e.id} 
       handleChoice={() => handleChoice(e.id)}
     />
   })
 
   function handleChoice(id){
+    setNewCard(false)
     const choice = currentPair.find(e => e.id === id)
     const notChoice = currentPair.find(e => e.id !== id)
-    const isChoiceAlreadyRanked = currentPair.some(e => e.value === choice)
-     //if the current choice is unranked
-     // check to see if range is the same
-     // if so, insert the choice into ranked array
-     // if not
-     // 
-     // change the values of max range
-
+    // console.log(typeof rankedArray)
+    const isChoiceAlreadyRanked = rankedArray.some(e => e === choice.value)
     
-    console.log(choice)
     if(isChoiceAlreadyRanked){
-      setMinRange(rankedArray.indexOf(choice.value))
+      setMinRange(rankedArray.indexOf(choice.value) + 1)
     }else{
-      setMaxRange(rankedArray).indexOf(choice.value)
+      setMaxRange(rankedArray.indexOf(notChoice.value))
     }
-    if(maxRange === minRange ){
-      rankedArray.splice(minRange, 0, choice.value)
-    }else{
-      const range = maxRange - minRange
-      const rangeMidpoint = Math.floor(range / 2) + minRange
-      setCurrentPair([
-        {
-          value: randomizedArray[0],
-          id: nanoid()
-        },
-        {
-          value: rankedArray[rangeMidpoint],
-          id: nanoid()
-        }
-      ])
-    }
+    // if(maxRange === minRange ){
+    //   const newRankedArray = [...rankedArray]
+    //   newRankedArray.splice(minRange, 0, choice.value)
+    //   setRankedArray(newRankedArray)
+    //   setRandomizedArray(prevRandomizedArray => prevRandomizedArray.slice(1))
+      
+    // }
   }
 
 
